827b85d956ed9896d584704d2f41f5dc88d6d31f
ArmanKhalili82 <hykhalili@yahoo.com>
2024-04-30 19:49:16 +0200

    Services Implemented

---
+++ GolMohammadi.Business/IService.cs
@@ -0,0 +1,26 @@
+﻿using GolmohammadiProject.Models.Product;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GolMohammadi.Business;
+
+public interface IService
+{
+    Task<List<ProductGroup>> GetAllProductGroup();
+    Task<List<SubGroup>> GetAllSubGroup();
+    Task<List<Product>> GetAllProduct();
+    Task CreateProductGroup (ProductGroup productGroup);
+    Task CreateSubGroup (SubGroup subGroup);
+    Task CreateProduct(Product product);
+    Task EditProductGroup(ProductGroup productGroup);
+    Task EditSubGroup(SubGroup subGroup);
+    Task EditProduct(Product product);
+    void Edit(int id);
+    void Delete(int id);
+    Task DeleteEditProductGroup(int id);
+    Task DeleteSubGroup(int id);
+    Task DeleteProduct(int id);
+}
--- GolMohammadi.Business/Models/Product/SubGroup.cs
+++ GolMohammadi.Business/Models/Product/SubGroup.cs
@@ -1,7 +1,12 @@
-﻿namespace GolmohammadiProject.Models.Product;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GolmohammadiProject.Models.Product;
 
 public class SubGroup
 {
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int Id { get; set; }
     public string Name { get; set; }
     public int ProductGroupId { get; set; }
