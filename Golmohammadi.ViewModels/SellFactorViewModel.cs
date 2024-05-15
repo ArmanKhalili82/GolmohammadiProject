114c47fe9ef44424cf85a88127716464255f06e0
ArmanKhalili82 <hykhalili@yahoo.com>
2024-05-15 18:48:27 +0200

    viewModel Added

---
+++ Golmohammadi.ViewModels/SellFactorViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Golmohammadi.ViewModels
+{
+    public class SellFactorViewModel
+    {
+        public CustomerViewModel Customer { get; set; }
+    }
+}
