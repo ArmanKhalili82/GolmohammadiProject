80519eb879e3e0271e6c30157068cec348693960
ArmanKhalili82 <hykhalili@yahoo.com>
2024-05-17 14:56:06 +0200

    factor

--- Golmohammadi.ViewModels/SellFactorViewModel.cs
+++ Golmohammadi.ViewModels/SellFactorViewModel.cs
@@ -8,8 +8,10 @@ namespace Golmohammadi.ViewModels
 {
     public class SellFactorViewModel
     {
-        public List<CustomerViewModel> Customer { get; set; }
-        public List<ProductViewModel> Product { get; set; }
+        public CustomerViewModel Customer { get; set; }
+        public List<CustomerViewModel> Customers { get; set; }
+        public ProductViewModel Product { get; set; }
+        public List<ProductViewModel> Products { get; set; }
         public int PriceUnit { get; set; }
         public int Unit { get; set; }
         public int TotalPrice { get; set; }
