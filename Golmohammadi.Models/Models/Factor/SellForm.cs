8c368557b675aca31a5cc05fd6a0be5446191237
ArmanKhalili82 <hykhalili@yahoo.com>
2024-05-20 06:54:45 +0200

    items

--- Golmohammadi.Models/Models/Factor/SellForm.cs
+++ Golmohammadi.Models/Models/Factor/SellForm.cs
@@ -2,6 +2,7 @@
 using Golmohammadi.Models.Models.Product;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,7 @@ namespace Golmohammadi.Models.Models.Factor
         public int ProductId { get; set; }
         public List<Products> Products { get; set; }
         public int CustomerId { get; set; }
+        [ForeignKey(nameof(CustomerId))]
         public Customers customer { get; set; }
         public int Unit { get; set; }
         public int TotalPrice { get; set; }
--- GolmohammadiProject/wwwroot/js/SellForm.js
+++ GolmohammadiProject/wwwroot/js/SellForm.js
@@ -1,4 +1,58 @@
-﻿
+﻿$(document).ready(function () {
+    ShowCustomerData();
+    ShowProductData();
+})
+
+//Get Data
+function ShowCustomerData() {
+    $.ajax({
+        url: 'Factor/GetCustomer',
+        type: 'Get',
+        dataType: 'json',
+        contentType: 'application/json',
+        success: function (result, statu, xhr) {
+            var object = "";
+            $.each(result, function (index, item) {
+                object += '<tr>';
+                object += '<td>' + item.customer.name + '</td>';
+                object += '<td>' + item.customer.address + '</td>';
+                object += '<td>' + item.customer.city + '</td>';
+                object += '<td>' + item.customer.region + '</td>';
+                object += '</tr>';
+            });
+            $('#table_data_customer').html(object);
+        },
+        error: function () {
+            alert("Data Can Not Get");
+        }
+    });
+};
+
+function ShowProductData() {
+    $.ajax({
+        url: 'Factor/GetProduct',
+        type: 'Get',
+        dataType: 'json',
+        contentType: 'application/json',
+        success: function (result, statu, xhr) {
+            var object = "";
+            $.each(result, function (index, item) {
+                object += '<tr>';
+                object += '<td>' + item.product.name + '</td>';
+                object += '<td>' + item.priceUnit + '</td>';
+                object += '<td>' + item.unit + '</td>';
+                object += '<td>' + item.totalPrice + '</td>';
+                object += '</tr>';
+            });
+            $('#table_data_product').html(object);
+        },
+        error: function () {
+            alert("Data Can Not Get");
+        }
+    });
+};
+
+
 
 function Create() {
     var result = Validate();
